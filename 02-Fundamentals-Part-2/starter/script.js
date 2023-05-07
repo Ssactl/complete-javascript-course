27ee91d18a22d69fa4c7d46daa3561419d0e16ff
jiani <1285092865@qq.com>
2023-05-07 18:30:10 +0200

    032-038

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,63 @@
+//-----------------------activating strict mode
+//help us to write a secure javascript code
+//and it has to be the very first statement in the script
+//1.strcit mode forbids us to do certain things
+//2.it creates visibal errors and the developer console. where in other situations
+//javascript would just fail silently
+//3.it introduces a short list of variable names that are reserved for features that might be added to the language a bit later
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true;
+}
+
+if (hasDriversLicense) {
+  console.log("i can drive :D");
+}
+
+// const interface = "Audio";
+const private = 1;
+const if=2;
+
+//-----------------------Functions
+//the fundemental building block of js are functions
+
+//-----------------------Function declarations vs. expressions
+//declaration, with function keyword, just like declaring a variable
+
+const age1=calAge1(1991);
+
+function calAge1(birthYeah){
+  return 2037-birthYeah;
+}
+
+console.log(age);
+
+//expression, with = operator, assgin a function to a variable
+//function is a value, too
+const calAge2=function(birthYeah){//it's a anomynous function
+  return 2037-birthYeah;
+}
+const age2=calAge2(1991);
+
+//difference: we can call function before its declaration instead of expression
+//it is because of a process called hoisting
+
+//-----------------------Arrow function
+//it's a function expression acutally
+const calAge3=birthYeah => 2037-birthYeah;
+
+const yearsUntilRetirement=(birthYeah,firstname)=>{
+  const age=2037-birthYeah;
+  const retirement=65-age;
+  // return retirement;
+  return `${firstname} return in ${retirement}`;
+}
+
+//difference: arrow functions do not get a so-called this keyword
+
+//-----------------------
