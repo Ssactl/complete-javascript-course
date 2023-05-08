2cb523f41049a660a75f7c4c2213b0d184ae01fa
jiani <1285092865@qq.com>
2023-05-08 02:06:53 +0200

    039-040

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,7 +12,7 @@ let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) {
-  hasDriverLicense = true;
+  hasDriversLicense = true;
 }
 
 if (hasDriversLicense) {
@@ -20,8 +20,8 @@ if (hasDriversLicense) {
 }
 
 // const interface = "Audio";
-const private = 1;
-const if=2;
+// const private = 1;
+// const if=2;
 
 //-----------------------Functions
 //the fundemental building block of js are functions
@@ -29,35 +29,61 @@ const if=2;
 //-----------------------Function declarations vs. expressions
 //declaration, with function keyword, just like declaring a variable
 
-const age1=calAge1(1991);
+const age1 = calAge1(1991);
 
-function calAge1(birthYeah){
-  return 2037-birthYeah;
+function calAge1(birthYeah) {
+  return 2037 - birthYeah;
 }
 
-console.log(age);
+console.log(age1);
 
 //expression, with = operator, assgin a function to a variable
 //function is a value, too
-const calAge2=function(birthYeah){//it's a anomynous function
-  return 2037-birthYeah;
-}
-const age2=calAge2(1991);
+const calAge2 = function (birthYeah) {
+  //it's a anomynous function
+  return 2037 - birthYeah;
+};
+const age2 = calAge2(1991);
 
 //difference: we can call function before its declaration instead of expression
 //it is because of a process called hoisting
 
 //-----------------------Arrow function
 //it's a function expression acutally
-const calAge3=birthYeah => 2037-birthYeah;
+const calAge3 = (birthYeah) => 2037 - birthYeah;
 
-const yearsUntilRetirement=(birthYeah,firstname)=>{
-  const age=2037-birthYeah;
-  const retirement=65-age;
+const yearsUntilRetirement = (birthYeah, firstname) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
   // return retirement;
   return `${firstname} return in ${retirement}`;
-}
+};
 
 //difference: arrow functions do not get a so-called this keyword
 
-//-----------------------
+//-----------------------039 Introduction to Arrays
+//only primitive values are immutable
+//arrays is not primitive value, so it is mutable
+
+//-----------------------040 array methods
+const friends = ["michael", "steven", "peter"];
+
+//add elements
+const newLength = friends.push("jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+//remove elements
+const popped = friends.pop(); //Last
+console.log(friends);
+console.log(popped);
+
+friends.shift(); //First
+console.log(friends);
+
+console.log(friends.indexOf("steven"));
+
+console.log(friends.includes("steven"));
