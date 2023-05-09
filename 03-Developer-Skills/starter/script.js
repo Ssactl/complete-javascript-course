b35b7aead626fd396b5fd672efd5a950e4fb7a3a
jiani <1285092865@qq.com>
2023-05-09 15:17:09 +0200

    049-056

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,32 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const calAge = birthYear => 2017 - birthYear;
+
+console.log();
+
+//Live Server extension can reload brower automatically
+//a more profession workfolw is use Node.js and a npm package called Live Server
+//npm is the Node package manager, which is bascially a program to download tools
+
+//debugging
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    //C) FIX
+    value: Number(prompt('Degrees celsius:')),
+  };
+
+  //B) FIND
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+//A) IDENTIFY
+console.log(measureKelvin());
