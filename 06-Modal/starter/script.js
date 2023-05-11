f41ecfad1d59ff3dd4364d2129dd455560a7d568
ssactl <1285092865@qq.com>
2023-05-11 14:19:22 +0200

    080

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal=document.querySelector(".modal");
+const overlay=document.querySelector(".overlay");
+const btnCloseModal=document.querySelector(".close-modal");
+
+//querySelector, only the matched first one get selecteds
+const btnOpenModal=document.querySelectorAll(".show-modal");
+
+const openModal=function(){
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i=0;i<btnOpenModal.length;i++){
+    console.log(btnOpenModal[i].textContents);
+    btnOpenModal[i].addEventListener("click",openModal);
+}
+
+
+
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click',closeModal);
+
+
+
+
+
