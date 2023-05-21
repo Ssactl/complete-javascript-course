7671e25a89a29904812e5a84245efce77b45a1c5
ssactl <1285092865@qq.com>
2023-05-21 13:39:50 +0200

    119 coding chanllenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,6 +57,38 @@ const restaurant = {
 };
 
 /*
+//-------119 coding chanllenge #3
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1
+const events = [...new Set(gameEvents.values())];
+// const events = [...new Set([...gameEvents.values()])];
+console.log(events);
+//??????????????? Iterator and spread operator
+//2
+gameEvents.delete(64);
+
+//4
+for (const [key, value] of gameEvents.entries()) {
+  console.log(
+    `${key < 45 ? '[FIRST HALF]' : '[SECOND HALF]'} ${key}: ${value}`
+  );
+}
+*/
+
+/*
 //-------117 maps
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
