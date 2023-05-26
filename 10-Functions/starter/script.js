bb765fd35cae7f7e28d828cfcf824dc197ea8d6a
ssactl_jianishe <1285092865@qq.com>
2023-05-26 23:52:10 +0200

    135

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -74,3 +74,51 @@ const addTaxRate = function (rate) {
 };
 
 const addVAT2 = addTaxRate(0.23);
+
+//-----135 coding challeng #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0:JavaScript', '1:Python', '2:Rust', '3"C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    //Display a prompt window
+    // prompt(
+    //   `What is your favourite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3:C++`
+    // );
+    const answer = Number(
+      prompt(`${this.question}\n${this.options.join('\n')}`)
+    );
+    //Based on the input number, update the answers array
+    console.log(typeof answer);
+    if (typeof answer === 'number' && answer < this.options.length) {
+      this.answers[answer]++;
+      console.log(this.answers);
+    }
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers}`);
+    }
+  },
+};
+
+poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+const testData1 = {
+  answers: [5, 2, 3],
+};
+const testData2 = {
+  answers: [1, 5, 3, 9, 6, 1],
+};
+const displayResultsTest = poll.displayResults;
+displayResultsTest.call(testData1);
+displayResultsTest.call(testData1, 'string');
+displayResultsTest.call(testData2);
+displayResultsTest.call(testData2, 'string');
