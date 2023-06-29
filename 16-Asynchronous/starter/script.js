898484162578a3e8149b543c9bde5c4dadafffe9
ssactl_jianishe <1285092865@qq.com>
2023-06-29 15:12:41 +0200

    256

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,62 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// Coding challenge #1
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages[Object.keys(data.languages)[0]]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[Object.keys(data.currencies)[0]].name
+      }</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+function whereAmI(lat, lng) {
+  // reverse geocoding
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?json=1&auth=349881567513101339201x113516`
+  )
+    .then(response => {
+      // console.log(response);
+      if (!response.ok)
+        throw new Error(`Problem with gecoding ${response.status}}`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      if (!data.country) throw new Error(`${data.error.message}`);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(res => {
+      // console.log(res);
+      if (!res.ok) throw new Error(`Problem with country ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+    })
+    .catch(error => console.error(error));
+}
+
+// TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+// TEST COORDINATES 2: 19.037, 72.873
+// TEST COORDINATES 2: -33.933, 18.474
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
